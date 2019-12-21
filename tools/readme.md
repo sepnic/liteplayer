7c0377e6d0445f853961bb90614536b0138b6f82
azure <sysu.zqlong@gmail.com>
2019-12-21 23:53:48 +0800

    Add socket-upload tool
    Signed-off-by: azure <sysu.zqlong@gmail.com>

---
+++ tools/readme.md
@@ -0,0 +1,27 @@
+## socket_upload.py
+
+### 1. 目的作用
+
+通过 socket 上传语音数据到本地，数据上传前需上传 "GENIE_SOCKET_UPLOAD_START" 字符串，表示上传开始；数据上传后需上传 "GENIE_SOCKET_UPLOAD_END" 字符串，表示上传结束。设备端代码实现见 liteplayer_debug.c。
+一般用在声学录音测试，需要长时间收集录音数据；或者播放器解码后的 PCM 数据上传，以便定位播放器解码问题还是底层 ALSA 设备问题。
+
+### 2. 使用方法
+
+#### 2.1. 设备端接口说明
+
+1. socket_upload_config_enable(enable)：是否使能 socket upload 功能
+
+2. socket_upload_config_server(addr, port)：配置 socket server 网络地址和端口号，socket server 和设备端必须在同一个局域网内
+
+3. socket_upload_start()：创建 socket upload 线程和资源
+
+4. socket_upload_fill_data(handle, data, size)：填充数据到 socket upload 缓冲区
+
+5. socket_upload_stop(handle)：停止 socket upload，阻塞等待 socket upload 线程退出
+
+#### 2.2. socket server 启动说明
+
+1. python socket_upload.py
+
+2. 生成的文件在同一级目录下，文件名如 record-2019-12-06-19-49-57.pcm
+
---
+++ tools/socket_upload.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: LUOYUN <sysu.zqlong@gmail.com>
+
+import socket, threading, sys
+import datetime, time
+import SimpleHTTPServer, BaseHTTPServer, SocketServer
+from SocketServer import ThreadingMixIn
+
+socket_port = 22808
+http_port   = 12800
+
+DOWNLOAD_FILE = "spk_test.mp3"
+UPLOAD_FILE   = "record-16KHz-16bit-Mono.pcm"
+
+UPLOAD_START = "GENIE_SOCKET_UPLOAD_START"
+UPLOAD_END   = "GENIE_SOCKET_UPLOAD_END"
+
+class StartSimpleHTTPServer:
+    def __init__(self, port):
+        self.port = port
+
+    def start_server(self):
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        httpd = SocketServer.TCPServer(("", self.port), Handler)
+        httpd.serve_forever()
+
+def socket_callback_recv(conn):
+    global UPLOAD_FILE
+    while True:
+        data = conn.recv(1024)
+        if UPLOAD_START in data:
+            now_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            UPLOAD_FILE = "record-" + now_time + ".pcm"
+            print "-->GENIE_SOCKET_UPLOAD_START: %s" % UPLOAD_FILE
+        elif UPLOAD_END in data:
+            print "-->GENIE_SOCKET_UPLOAD_END: %s" % UPLOAD_FILE
+            index = data.find(UPLOAD_END)
+            if index > 0:
+                with open(UPLOAD_FILE.decode('utf-8'), 'ab+') as f:
+                    f.write(data[:index])
+            break
+        else:
+            with open(UPLOAD_FILE.decode('utf-8'), 'ab+') as f:
+                f.write(data)
+    conn.close()
+
+if __name__ == '__main__':
+    http_server = StartSimpleHTTPServer(http_port)
+    http_server_thread = threading.Thread(target=http_server.start_server)
+    http_server_thread.setDaemon(True)
+    http_server_thread.start()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('0.0.0.0', socket_port))
+    sock.listen(5)
+    while True:
+        conn, addr = sock.accept()
+        thread_recv = threading.Thread(target=socket_callback_recv, args=(conn, ))
+        thread_recv.setDaemon(True)
+        thread_recv.start()
