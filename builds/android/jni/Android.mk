0585db1aa4ab94a3b3a374558bfe2fa6009e3b37
sepnic <sysu.zqlong@gmail.com>
2021-10-02 13:24:07 +0800

    For aac content, use pv-aac decoder instead of fdk-aac

--- builds/android/jni/Android.mk
+++ builds/android/jni/Android.mk
@@ -23,17 +23,8 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 TOP_DIR := ${LOCAL_PATH}/../../..
 THIRDPARTY_FILES := $(wildcard  \
-    ${TOP_DIR}/thirdparty/speexdsp/resample.c \
     ${TOP_DIR}/thirdparty/codecs/mp3-pvmp3/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libAACdec/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libPCMutils/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libFDK/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libSYS/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libMpegTPDec/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libSBRdec/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libArithCoding/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libDRCdec/src/*.cpp \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libSACdec/src/*.cpp)
+    ${TOP_DIR}/thirdparty/codecs/aac-pvaac/*.cpp)
 THIRDPARTY_FILES := $(THIRDPARTY_FILES:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES := \
     ${THIRDPARTY_FILES} \
@@ -41,7 +32,7 @@ LOCAL_SRC_FILES := \
     ${TOP_DIR}/library/source/esp_adf/audio_event_iface.c \
     ${TOP_DIR}/library/source/audio_decoder/mp3_pvmp3_wrapper.c \
     ${TOP_DIR}/library/source/audio_decoder/mp3_decoder.c \
-    ${TOP_DIR}/library/source/audio_decoder/aac_fdk_wrapper.c \
+    ${TOP_DIR}/library/source/audio_decoder/aac_pvaac_wrapper.c \
     ${TOP_DIR}/library/source/audio_decoder/aac_decoder.c \
     ${TOP_DIR}/library/source/audio_decoder/m4a_decoder.c \
     ${TOP_DIR}/library/source/audio_decoder/wav_decoder.c \
@@ -63,18 +54,11 @@ LOCAL_C_INCLUDES += \
     ${TOP_DIR}/thirdparty/speexdsp \
     ${TOP_DIR}/thirdparty/codecs/mp3-pvmp3/include \
     ${TOP_DIR}/thirdparty/codecs/mp3-pvmp3/src \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libAACdec/include \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libPCMutils/include \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libFDK/include \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libSYS/include \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libMpegTPDec/include \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libSBRdec/include \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libArithCoding/include \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libDRCdec/include \
-    ${TOP_DIR}/thirdparty/codecs/aac-fdk/libSACdec/include
+    ${TOP_DIR}/thirdparty/codecs/aac-pvaac
 LOCAL_CFLAGS += -DOS_ANDROID
-LOCAL_CFLAGS += -DCONFIG_SPEEXDSP_FIXED_POINT -DENABLE_DECODER_AAC_SBR -DCONFIG_SINK_FIXED_S16LE
-LOCAL_CFLAGS += -Wall -Werror -Wno-error=unused-function -Wno-error=unused-variable
+LOCAL_CFLAGS += -DCONFIG_SINK_FIXED_S16LE
+LOCAL_CFLAGS += -Wall -Werror -Wno-error=unused-value -Wno-error=narrowing
+LOCAL_CFLAGS += -DOSCL_IMPORT_REF= -DOSCL_EXPORT_REF= -DOSCL_UNUSED_ARG=
 LOCAL_LDLIBS := -llog
 LOCAL_SHARED_LIBRARIES += sysutils
 LOCAL_MODULE := libliteplayer_core
