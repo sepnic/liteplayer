f5cfe3129e019ff7136085771586b44c1b1d2ca9
sepnic <sysu.zqlong@gmail.com>
2022-04-02 14:59:14 +0800

    Update android demo, sync with liteplayer_open project

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,5 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
+## mbedtls
+include $(CLEAR_VARS)
+TOP_DIR := ${LOCAL_PATH}/../..
+MBEDTLS_FILES := $(wildcard  ${TOP_DIR}/thirdparty/mbedtls/library/*.c)
+LOCAL_SRC_FILES := $(MBEDTLS_FILES:$(LOCAL_PATH)/%=%)
+LOCAL_C_INCLUDES += ${TOP_DIR}/thirdparty/mbedtls/include
+LOCAL_CFLAGS += -Wall -Werror
+LOCAL_CFLAGS += -D_SOCKLEN_T
+LOCAL_MODULE := libmbedtls
+include $(BUILD_SHARED_LIBRARY)
+
 ## sysutils
 include $(CLEAR_VARS)
 TOP_DIR := ${LOCAL_PATH}/../..
@@ -11,22 +22,27 @@ LOCAL_SRC_FILES := \
     ${TOP_DIR}/thirdparty/sysutils/source/cutils/memdbg.c \
     ${TOP_DIR}/thirdparty/sysutils/source/cutils/mlooper.c \
     ${TOP_DIR}/thirdparty/sysutils/source/cutils/mqueue.c \
-    ${TOP_DIR}/thirdparty/sysutils/source/cutils/ringbuf.c
-LOCAL_C_INCLUDES += ${TOP_DIR}/thirdparty/sysutils/include
+    ${TOP_DIR}/thirdparty/sysutils/source/cutils/ringbuf.c \
+    ${TOP_DIR}/thirdparty/sysutils/source/httpclient/httpclient.c
+LOCAL_C_INCLUDES += \
+    ${TOP_DIR}/thirdparty/mbedtls/include \
+    ${TOP_DIR}/thirdparty/sysutils/include
 LOCAL_CFLAGS += -Wall -Werror -DOS_ANDROID
+LOCAL_CFLAGS += -DSYSUTILS_HAVE_MBEDTLS_ENABLED
 LOCAL_LDLIBS := -llog
+LOCAL_SHARED_LIBRARIES += mbedtls
 LOCAL_MODULE := libsysutils
 include $(BUILD_SHARED_LIBRARY)
 
 ## liteplayercore
 include $(CLEAR_VARS)
 TOP_DIR := ${LOCAL_PATH}/../..
-THIRDPARTY_FILES := $(wildcard  \
+CODEC_FILES := $(wildcard  \
     ${TOP_DIR}/thirdparty/codecs/mp3-pvmp3/src/*.cpp \
     ${TOP_DIR}/thirdparty/codecs/aac-pvaac/*.cpp)
-THIRDPARTY_FILES := $(THIRDPARTY_FILES:$(LOCAL_PATH)/%=%)
+CODEC_FILES := $(CODEC_FILES:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES := \
-    ${THIRDPARTY_FILES} \
+    ${CODEC_FILES} \
     ${TOP_DIR}/library/source/esp_adf/audio_element.c \
     ${TOP_DIR}/library/source/esp_adf/audio_event_iface.c \
     ${TOP_DIR}/library/source/audio_decoder/mp3_pvmp3_wrapper.c \
@@ -54,7 +70,7 @@ LOCAL_C_INCLUDES += \
     ${TOP_DIR}/thirdparty/codecs/mp3-pvmp3/include \
     ${TOP_DIR}/thirdparty/codecs/mp3-pvmp3/src \
     ${TOP_DIR}/thirdparty/codecs/aac-pvaac
-LOCAL_CFLAGS += -Wall -Werror -DOS_ANDROID
+LOCAL_CFLAGS += -Wall -Werror
 LOCAL_CFLAGS += -DLITEPLAYER_CONFIG_SINK_FIXED_S16LE -DLITEPLAYER_CONFIG_AAC_SBR
 LOCAL_CFLAGS += -Wno-error=narrowing
 LOCAL_CFLAGS += -Wno-error=implicit-const-int-float-conversion
@@ -69,21 +85,15 @@ include $(BUILD_SHARED_LIBRARY)
 ## liteplayeradapter
 include $(CLEAR_VARS)
 TOP_DIR := ${LOCAL_PATH}/../..
-THIRDPARTY_FILES := $(wildcard  ${TOP_DIR}/thirdparty/mbedtls/library/*.c \
-                                ${TOP_DIR}/thirdparty/sysutils/source/httpclient/*.c)
-THIRDPARTY_FILES := $(THIRDPARTY_FILES:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES := \
-    ${THIRDPARTY_FILES} \
     ${TOP_DIR}/adapter/source_httpclient_wrapper.c \
     ${TOP_DIR}/adapter/source_file_wrapper.c \
     ${TOP_DIR}/adapter/sink_opensles_wrapper.cpp
 LOCAL_C_INCLUDES += \
     ${TOP_DIR}/library/include \
     ${TOP_DIR}/adapter \
-    ${TOP_DIR}/thirdparty/sysutils/include \
-    ${TOP_DIR}/thirdparty/mbedtls/include
-LOCAL_CFLAGS += -Wall -Werror -DOS_ANDROID
-LOCAL_CFLAGS += -D_SOCKLEN_T -DSYSUTILS_HAVE_MBEDTLS_ENABLED
+    ${TOP_DIR}/thirdparty/sysutils/include
+LOCAL_CFLAGS += -Wall -Werror
 LOCAL_LDLIBS := -llog -lOpenSLES
 LOCAL_SHARED_LIBRARIES += sysutils
 LOCAL_MODULE := liteplayer_adapter
